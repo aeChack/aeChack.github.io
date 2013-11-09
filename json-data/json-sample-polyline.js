670e876e382c35fef7b889587ac1ed67907914d7
Theo Armour <t.armour@gmail.com>
2013-11-09 15:10:03 -0800

    sample update

--- json-data/json-sample-polyline.js
+++ json-data/json-sample-polyline.js
@@ -1,11 +1,10 @@
 polyline = {
 	"vertices" : {
-		[0,0,],
+		[0,0,0],
 		[0,100,0],
 		[100,100,0],
 		[100,0,0]
 	},
-	"buildingHeight": 120,
 	"numberFloors": "none",
 	"buildingType": "office"
 }	
