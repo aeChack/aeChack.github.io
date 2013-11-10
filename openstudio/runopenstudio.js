042e6fb102cc22b90e852c217201c8e1d707b655
Ubuntu <ubuntu@ip-172-31-41-248.us-west-2.compute.internal>
2013-11-10 05:35:11 +0000

    updates

--- openstudio/runopenstudio.js
+++ openstudio/runopenstudio.js
@@ -1,11 +1,41 @@
-var assert = require('assert')
+var assert = require('assert');
 var openstudio = require('OpenStudio.js').openstudio;
 
 var polyline = require('/home/ubuntu/dev/aeChack.github.io/openstudio/polyline.json');
 
-model = openstudio.model.Model();
+model = new openstudio.model.Model();
 
-var osmpath = new openstudio.path("~/aeChack.osm");
+// set building story height
+var floorHeight = 3.5;
+
+// set the number of stories
+var stories = 10;
+
+// create each building story
+for (var i=0; i<stories; i++)
+{
+	// create story
+	var zCoord = i * floorHeight;
+	var story = new openstudio.model.BuildingStory(model);
+	story.setNominalZCoordinate(zCoord);
+	story.setNominalFloortoFloorHeight(floorHeight);
+        
+	// create floor prints
+	var floorPrint = new openstudio.Point3dVector();
+
+        for (var j = 0; j < polyline.vertices.length; j++)
+        {
+		floorPrint.add(new openstudio.Point3d(polyline.vertices[j][0],polyline.vertices[j][1],polyline.vertices[j][2]));
+	}
+        
+        // create spaces	
+	var thermalZone = new openstudio.model.ThermalZone(model);
+	var space = openstudio.model.Space.fromFloorPrint(floorPrint, floorHeight, model);
+	space.get().setThermalZone(thermalZone);
+}
+
+// set file path location and save out model
+var osmpath = new openstudio.path("aeChack.osm");
 console.log("Saving OSM to " + openstudio.toString(osmpath));
 
 model.save(osmpath, true);
