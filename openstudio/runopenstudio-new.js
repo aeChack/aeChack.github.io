c9bfcb8dca25d0eaab3c9bb3b9d7ed397115699a
Alan Jackson <a.jackson@case-inc.com>
2013-11-10 12:02:52 -0800

    updates

--- openstudio/runopenstudio-new.js
+++ openstudio/runopenstudio-new.js
@@ -78,7 +78,7 @@ function addExampleConstructions(model)
   var exteriorWall = new openstudio.model.Construction(model);
   exteriorWall.setName("Exterior Wall");
   exteriorWall.setInsulation(i02_50mm_insulation_board);
-  exteriorSurfaceConstructions.setWallConstruction(exteriorWall);
+  exteriorSurfaceConstructions.setWallConstruction(opaqueMaterials);
   opaqueMaterials.clear();
   
   console.log("Finished Exterior Wall Construction");
@@ -284,57 +284,57 @@ function addExampleConstructions(model)
 
 function createGeometry(model)
 {
-	var polyline = require('/home/ubuntu/dev/aeChack.github.io/openstudio/polyline.json');
+  var polyline = require('/home/ubuntu/dev/aeChack.github.io/openstudio/polyline.json');
 
-	// add constructions
-	addExampleConstructions(model);
-	var defaultConstructionSet = openstudio.model.getDefaultConstructionSets(model).get(0);
-	
-	// set building story height
-	var floorHeight = 3.5;
+  // add constructions
+  addExampleConstructions(model);
+  var defaultConstructionSet = openstudio.model.getDefaultConstructionSets(model).get(0);
+  
+  // set building story height
+  var floorHeight = 3.5;
 
-	// set the number of stories
-	var stories = 10;
+  // set the number of stories
+  var stories = 10;
 
-	// create each building story element
-	for (var i=0; i<stories; i++)
-	{
-		// create story
-		var zCoord = i * floorHeight;
-		var story = new openstudio.model.BuildingStory(model);
-		story.setNominalZCoordinate(zCoord);
-		story.setNominalFloortoFloorHeight(floorHeight);
-	        
-		// create floor prints
-		var floorPrint = new openstudio.Point3dVector();
+  // create each building story element
+  for (var i=0; i<stories; i++)
+  {
+    // create story
+    var zCoord = i * floorHeight;
+    var story = new openstudio.model.BuildingStory(model);
+    story.setNominalZCoordinate(zCoord);
+    story.setNominalFloortoFloorHeight(floorHeight);
+          
+    // create floor prints
+    var floorPrint = new openstudio.Point3dVector();
 
         for (var j = 0; j < polyline.vertices.length; j++)
         {
-        	floorPrint.add(new openstudio.Point3d(polyline.vertices[j][0],polyline.vertices[j][1],polyline.vertices[j][2]));
-		}
-	        
-        // create spaces	
-		var thermalZone = new openstudio.model.ThermalZone(model);
-		var space = openstudio.model.Space.fromFloorPrint(floorPrint, floorHeight, model);
-		space.get().setThermalZone(thermalZone);
-	}
+          floorPrint.add(new openstudio.Point3d(polyline.vertices[j][0],polyline.vertices[j][1],polyline.vertices[j][2]));
+    }
+          
+        // create spaces  
+    var thermalZone = new openstudio.model.ThermalZone(model);
+    var space = openstudio.model.Space.fromFloorPrint(floorPrint, floorHeight, model);
+    space.get().setThermalZone(thermalZone);
+  }
 }
 
 function saveModel(model)
 {
-	// set file path location and save out model
-	var osmpath = new openstudio.path("aeChack.osm");
-	console.log("Saving OSM to " + openstudio.toString(osmpath));
+  // set file path location and save out model
+  var osmpath = new openstudio.path("aeChack.osm");
+  console.log("Saving OSM to " + openstudio.toString(osmpath));
 
-	model.save(osmpath, true);
+  model.save(osmpath, true);
 }
 
 function createModel()
 {
-	model = new openstudio.model.Model();
-	createGeometry(model);
-	saveModel(model);
-	return model 
+  model = new openstudio.model.Model();
+  createGeometry(model);
+  saveModel(model);
+  return model 
 }
 
 createModel();
